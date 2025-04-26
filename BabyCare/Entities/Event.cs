f1c2514dedcf7ba7f6af7e19e584041e6438117b
Mehmet Polat <mehwetpolat@gmail.com>
2025-04-26 18:14:54 +0300

    evetn partial and event page has been created

---
+++ BabyCare/Entities/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace BabyCare.Entities
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public string EventImageUrl { get; set; }
+        public string EventCity { get; set; }
+        public string EventDate { get; set; }
+        public string EventTime { get; set; }
+    }
+}
---
+++ BabyCare/ViewComponents/_DefaultEventComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using BabyCare.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabyCare.ViewComponents
+{
+    public class _DefaultEventComponentPartial: ViewComponent
+    {
+        private readonly BabyCareContext _context;
+
+        public _DefaultEventComponentPartial(BabyCareContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Events.ToList();
+            return View(values);
+        }
+    }
+}
