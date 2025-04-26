14b8e134c9f1e378c6bd332c945eb4e2f642caa6
Mehmet Polat <mehwetpolat@gmail.com>
2025-04-26 18:16:51 +0300

    testimonial partial and page has been created

---
+++ BabyCare/ViewComponents/_DefaultTestimonialComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using BabyCare.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabyCare.ViewComponents
+{
+    public class _DefaultTestimonialComponentPartial: ViewComponent
+    {
+        private readonly BabyCareContext _context;
+
+        public _DefaultTestimonialComponentPartial(BabyCareContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Testimonials.ToList();
+            return View(values);
+        }
+    }
+}
